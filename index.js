f161e0b0aa42b1e3e1bb2b23916f110babd9b9b1
João Felipe <joaofelipe.beta@gmail.com>
2026-04-27 17:09:33 -0300

    add rota /

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.static("."));
 
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta: ${PORT}`);
 });
